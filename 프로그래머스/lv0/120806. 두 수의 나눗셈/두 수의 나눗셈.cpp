0c7cd3d0b92f26830c8ca233dff6a214a60a44b9
Jungmin5 <100908119+Jungmin5@users.noreply.github.com>
2023-06-19 03:57:54 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    int answer = 0;
+    if (num1 > 0 && num1 <= 100 && num2 > 0 && num2 <= 100) {
+        answer = ((double)num1 / (double)num2) * 1000;
+    }
+    return answer;
+}
