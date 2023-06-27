0b22d80c9c1f4992d72764ad71c52b6b390667a5
Jungmin5 <100908119+Jungmin5@users.noreply.github.com>
2023-06-28 04:17:31 +0900

    [Bronze II] Title: OX퀴즈, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+for i in range(n):
+    s = list(input())
+    score, count = 0, 1
+    for j in s:
+        if j == 'O':
+            score += count
+            count += 1
+        else:
+            count = 1
+    print(score)
