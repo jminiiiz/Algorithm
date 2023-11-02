682dd86013e0cdfeb12f84138311ab3bca84fa6c
Jungmin5 <100908119+Jungmin5@users.noreply.github.com>
2023-11-03 02:05:01 +0900

    안전 영역.cc

--- 백준/Silver/2468. 안전 영역/안전 영역.cc
+++ 백준/Silver/2468. 안전 영역/안전 영역.cc
@@ -1,32 +1,34 @@
-#include<bits/stdc++.h>
-using namespace std;   
-int a[101][101], e[101][101], visited[101][101], n, temp, ret = 1;
-int dy[4] = {-1, 0, 1, 0}, dx[4] = {0, 1, 0, -1};
-void dfs(int y, int x, int d){
-    visited[y][x] = 1; 
-    for(int i = 0; i < 4; i++){
-        int ny = y + dy[i]; 
-        int nx = x + dx[i]; 
-        if(ny < 0 || nx < 0 || ny >= n || nx >= n) continue; 
-        if(!visited[ny][nx] && a[ny][nx] > d) dfs(ny, nx, d);
+#include <bits/stdc++.h>
+using namespace std;
+
+int a[100][100], e[100][100], visited[100][100], n, tmp, ret = 1;
+int dy[4] = {-1, 0, 1, 0} , dx[4] = {0, 1, 0, -1};
+
+void dfs(int x, int y, int d) {
+    visited[x][y] = 1;
+    for (int i = 0; i < 4; i++) {
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+        if (nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
+        if (!visited[nx][ny] && a[nx][ny] > d) dfs(nx, ny, d);
     }
     return;
-} 
-int main(){
+}
+
+int main() {
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);   
-    cin >> n; 
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
             cin >> a[i][j];
         }
     }
-    for(int d = 1; d < 101; d++){
-        fill(&visited[0][0], &visited[0][0] + 101 * 101, 0); 
-        int cnt = 0;  
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
+    for (int d = 1; d < 101; d++) {
+        fill(&visited[0][0], &visited[0][0] + 100 * 100, 0);
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 if(a[i][j] > d && !visited[i][j]) {
                     dfs(i, j, d);
                     cnt++;
@@ -35,6 +37,6 @@ int main(){
         }
         ret = max(ret, cnt);
     }
-    cout << ret << '\n';
-    return 0; 
+    cout << ret << "\n";
+    return 0;
 }
