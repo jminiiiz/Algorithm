975e5784039a1ceb607cc13f1e6bcac23ca69e94
Jungmin5 <100908119+Jungmin5@users.noreply.github.com>
2023-11-04 07:42:12 +0900

    [level unrated] Title: 문자열 섞기, Time: 0.01 ms, Memory: 3.63 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string solution(string str1, string str2) {
+    string s = "";
+    for (int i = 0; i < str1.size(); i++) {
+        s += str1[i];
+        s += str2[i];
+    }
+    return s;
+}
