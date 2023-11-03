1016f573165dd2cab5227686fae4f61eebb6201d
Jungmin5 <100908119+Jungmin5@users.noreply.github.com>
2023-11-04 08:18:48 +0900

    [level unrated] Title: 공배수, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/unrated/181936. 공배수/공배수.cpp
+++ 프로그래머스/unrated/181936. 공배수/공배수.cpp
@@ -1,8 +1,3 @@
-#include <string>
-#include <vector>
-
-using namespace std;
-
 int solution(int number, int n, int m) {
-	return !(number % n) && !(number % m);
+	return !(number % n) & !(number % m);
 }
