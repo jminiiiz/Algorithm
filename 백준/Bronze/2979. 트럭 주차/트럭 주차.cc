db26aa22877929c8c632a203e5eb50e4e7b6da49
Jungmin5 <100908119+Jungmin5@users.noreply.github.com>
2024-01-28 01:51:08 +0900

    [Bronze II] Title: 트럭 주차, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2979. 트럭 주차/트럭 주차.cc
+++ 백준/Bronze/2979. 트럭 주차/트럭 주차.cc
@@ -1,18 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int A, B, C, T1, T2, cnt[104], ret;
+int A, B, C, a, b, arr[104], ret;
 int main() {
     cin >> A >> B >> C;
     for (int i = 0; i < 3; i++) {
-        cin >> T1 >> T2;
-        for (int j = T1; j < T2; j++) cnt[j]++;
+        cin >> a >> b;
+        for (int j = a; j < b; j++) arr[j]++;
     }
-    for (int j = 0; j < 100; j++) {
-        if (cnt[j] == 1) ret += A;
-        else if (cnt[j] == 2) ret += B * 2;
-        else if (cnt[j] == 3) ret += C * 3;
+    for (int i = 0; i < 104; i++) {
+        if (true) {
+            if (arr[i] == 1) ret += A;
+            else if (arr[i] == 2) ret += B * 2;
+            else if (arr[i] == 3) ret += C * 3;
+        }
     }
-    cout << ret << "\n";
+    cout << ret;
     return 0;
 }
