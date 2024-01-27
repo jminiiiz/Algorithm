650ba1c5520e2491205d4c4beec17d0c1849b810
Jungmin5 <100908119+Jungmin5@users.noreply.github.com>
2024-01-28 01:27:05 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -1,16 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
-string str;
-int cnt[26];
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    cin >> str;
-    for (char a : str) {
-        cnt[a - 'a']++;
-    }
-    for (int i = 0; i < 26; i++) cout << cnt[i] << " ";
 
+int a[26];
+string s;
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin >> s;
+    for (char c : s) a[c-97]++;
+    for (int i = 0; i < 26; i++) cout << a[i] << " ";
     return 0;
 }
