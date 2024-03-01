8222b768e8b5a610bfa4755ccf50ba4fdb563a6a
Jungmin5 <100908119+Jungmin5@users.noreply.github.com>
2024-03-02 05:31:59 +0900

    [Bronze V] Title: 오늘 날짜, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10699. 오늘 날짜/오늘 날짜.cc
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.cc
@@ -3,5 +3,8 @@ using namespace std;
 
 int main()
 {
-    cout << "2024-03-02";   
+    time_t timer = time(nullptr);
+    struct tm* t = localtime((&timer));
+
+        printf("%d-%02d-%02d",t->tm_year + 1900, t->tm_mon + 1, t->tm_mday + 1); // 서버시간과 현재시간 차이로 인해 날짜에 + 1
 }
