6bba685fe0eb3af44b889d8459e3587cf42b3693
Jungmin5 <100908119+Jungmin5@users.noreply.github.com>
2024-03-07 03:55:48 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int T, A, B;
+int main() {
+    cin >> T;
+    for (int i = 0; i < T; i++) {
+        cin >> A >> B;
+        cout << A + B << "\n";
+    }
+}
