cc0b740cb036cd41090a544f41312336dd886a99
Jungmin5 <100908119+Jungmin5@users.noreply.github.com>
2024-03-12 01:33:43 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2204 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,31 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int max_n = 104;
-int dy[4] = {-1, 0, 1, 0};
-int dx[4] = {0, 1, 0, -1};
-int n, m, a[max_n][max_n], visited[max_n][max_n], y, x;
+#define MAX 104
+string grid[MAX];
+int visited[MAX][MAX];
+int N, M, x, y;
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
 int main() {
-    scanf("%d %d", &n, &m);
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            scanf("%1d", &a[i][j]);
-        }
-    }
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    cin >> N >> M;
+    for (int i = 0; i < N; i++) cin >> grid[i];
+    for (int i = 0; i < N; i++) fill(visited[i], visited[i] + M, -1);
     queue<pair<int, int>> q;
-    visited[0][0] = 1;
     q.push({0, 0});
-    while (q.size()) {
-        tie(y, x) = q.front(); q.pop();
+    visited[0][0] = 0;
+    while (!q.empty()) {
+        tie(x, y) = q.front(); q.pop();
         for (int i = 0; i < 4; i++) {
-            int ny = y + dy[i];
             int nx = x + dx[i];
-            if (ny < 0 || ny >= n || nx >= m || a[ny][nx] == 0) continue;
-            if (visited[ny][nx]) continue;
-            visited[ny][nx] = visited[y][x] + 1;
-            q.push({ny, nx});
+            int ny = y + dy[i];
+            if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;;
+            if (visited[nx][ny] >= 0 || grid[nx][ny] != '1') continue;
+            visited[nx][ny] = visited[x][y] + 1;
+            q.push({nx, ny});
         }
     }
-    printf("%d", visited[n-1][m-1]);
-    return 0;
-    
+    cout << visited[N-1][M-1] + 1;
 }
