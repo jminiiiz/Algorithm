69820d42726f76dc47e883ce6b001846f44e90ad
Jungmin5 <100908119+Jungmin5@users.noreply.github.com>
2024-03-13 04:09:32 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    string s;
+    cin >> T;
+    for (int i = 0; i < T; i++) {
+        cin >> s;
+        cout << s[0] << s[s.length() - 1] << "\n";
+    }
+}
