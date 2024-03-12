9d69502d378d7c861922298a431353355df0e787
Jungmin5 <100908119+Jungmin5@users.noreply.github.com>
2024-03-13 03:44:28 +0900

    [Bronze V] Title: 학점계산, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s;
+    double d = 0.0;
+    cin >> s;
+
+    switch (s[0]) {
+        case 'A':
+            d = 4.0;
+            break;
+        case 'B':
+            d = 3.0;
+            break;
+        case 'C':
+            d = 2.0;
+            break;
+        case 'D':
+            d = 1.0;
+            break;
+    }
+    switch (s[1]) {
+        case '+':
+            d += 0.3;
+            break;
+        case '-':
+            d -= 0.3;
+            break;
+    }
+    printf("%.1f", d);
+}
