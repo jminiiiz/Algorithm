971fe21565d5efe1ea05740686b3b273f0953a8e
Jungmin5 <100908119+Jungmin5@users.noreply.github.com>
2024-03-13 03:54:44 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.cc
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.cc
@@ -1,17 +1,11 @@
 #include <iostream>
-#include <string>
 using namespace std;
-
+string s;
 int main() {
-    string s;
     cin >> s;
-    for (int i = 0; i < s.size(); i++) {
-        if (s[i] >= 97 && s[i] <= 122) {
-            s[i] -= 32;
-        }
-        else if (s[i] >= 65 && s[i] <= 90) {
-            s[i] += 32;
-        }
-        cout << s[i];
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] >= 'a' and s[i] <= 'z') s[i] -= 32;
+        else s[i] += 32;
     }
+    cout << s;
 }
