63e95ab2b054569f0603bf46f051cae19b50b00c
Jungmin5 <100908119+Jungmin5@users.noreply.github.com>
2024-03-20 04:07:30 +0900

    [Bronze IV] Title: 세수정렬, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.cc
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int a, b, c;
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> a >> b >> c;
+    cout << min({a, b, c}) << " " << a+b+c-max({a,b,c})-min({a,b,c}) << " " << max({a,b,c});
+}
