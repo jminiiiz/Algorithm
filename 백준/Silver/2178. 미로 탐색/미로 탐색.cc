6db7c6d284ce0eee2f2212b9818bcccf3bbe2bf6
Jungmin5 <100908119+Jungmin5@users.noreply.github.com>
2024-03-22 16:58:41 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2200 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,29 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define MAX 104
-string grid[MAX];
-int visited[MAX][MAX];
-int N, M, x, y;
+string board[102];
+int dist[102][102];
+int n, m;
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
 int main() {
-    ios::sync_with_stdio(false); cin.tie(NULL);
-    cin >> N >> M;
-    for (int i = 0; i < N; i++) cin >> grid[i];
-    for (int i = 0; i < N; i++) fill(visited[i], visited[i] + M, -1);
-    queue<pair<int, int>> q;
+    ios::sync_with_stdio(0); cin.tie(0);
+    cin >> n >> m;
+    for(int i = 0; i < n; i++)
+        cin >> board[i];
+    for(int i = 0; i < n; i++) fill(dist[i],dist[i] + m,-1);
+    queue<pair<int,int> > q;
     q.push({0, 0});
-    visited[0][0] = 0;
-    while (!q.empty()) {
-        tie(x, y) = q.front(); q.pop();
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;;
-            if (visited[nx][ny] >= 0 || grid[nx][ny] != '1') continue;
-            visited[nx][ny] = visited[x][y] + 1;
+    dist[0][0] = 0;
+    while(!q.empty()){
+        auto cur = q.front(); q.pop();
+        for(int dir = 0; dir < 4; dir++){
+            int nx = cur.first + dx[dir];
+            int ny = cur.second + dy[dir];
+            if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+            if(dist[nx][ny] >= 0 || board[nx][ny] != '1') continue;
+            dist[nx][ny] = dist[cur.first][cur.second] + 1;
             q.push({nx, ny});
         }
     }
-    cout << visited[N-1][M-1] + 1;
+    cout << dist[n-1][m-1] + 1;
 }
