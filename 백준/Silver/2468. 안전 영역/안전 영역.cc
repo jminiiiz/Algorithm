5149c3ff6fde8a7a5bea6dc8a4123e2256c8c09e
Jungmin5 <100908119+Jungmin5@users.noreply.github.com>
2024-03-24 05:42:22 +0900

    [Silver I] Title: 안전 영역, Time: 20 ms, Memory: 2072 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.cc
+++ 백준/Silver/2468. 안전 영역/안전 영역.cc
@@ -1,42 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int a[100][100], e[100][100], visited[100][100], n, tmp, ret = 1;
-int dy[4] = {-1, 0, 1, 0} , dx[4] = {0, 1, 0, -1};
-
-void dfs(int x, int y, int d) {
-    visited[x][y] = 1;
-    for (int i = 0; i < 4; i++) {
-        int nx = x + dx[i];
-        int ny = y + dy[i];
-        if (nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
-        if (!visited[nx][ny] && a[nx][ny] > d) dfs(nx, ny, d);
+int board[102][102];
+bool vis[102][102];
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
+int n, maxlimit, maxcnt;
+void bfs(int x, int y, int limit) {
+    queue<pair<int, int>> q;
+    vis[x][y] = true;
+    q.push({x, y});
+    while (!q.empty()) {
+        auto cur = q.front(); q.pop();
+        for (int dir = 0; dir < 4; dir++) {
+            int nx = cur.first + dx[dir];
+            int ny = cur.second + dy[dir];
+            if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+            if (vis[nx][ny] == 0 && board[nx][ny] > limit) {
+                vis[nx][ny] = true;
+                q.push({nx, ny});
+            }
+        }
     }
-    return;
 }
-
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
+    ios::sync_with_stdio(0); cin.tie(0);
     cin >> n;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            cin >> a[i][j];
+            cin >> board[i][j];
+            maxlimit = max(board[i][j], maxlimit);
         }
     }
-    for (int d = 1; d < 101; d++) {
-        fill(&visited[0][0], &visited[0][0] + 100 * 100, 0);
+    for (int limit = 0; limit <= maxlimit; limit++) {
+        for (int i = 0; i < n; i++) {
+            fill(vis[i], vis[i] + n, 0);
+        }
         int cnt = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if(a[i][j] > d && !visited[i][j]) {
-                    dfs(i, j, d);
+                if (board[i][j] > limit && !vis[i][j]) {
+                    bfs(i, j, limit);
                     cnt++;
                 }
             }
         }
-        ret = max(ret, cnt);
+        maxcnt = max(cnt, maxcnt);
     }
-    cout << ret << "\n";
-    return 0;
+    cout << maxcnt;
 }
