ae498d4768b3ae2f39a3a9ee81491677c3bc842a
Jungmin5 <100908119+Jungmin5@users.noreply.github.com>
2024-03-25 05:24:05 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 0 ms, Memory: 2100 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, m;
+int arr1[101][101], arr2[101][101];
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> arr1[i][j];
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> arr2[i][j];
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << arr1[i][j] + arr2[i][j] << ' ';
+        }
+        cout << "\n";
+    }
+}
