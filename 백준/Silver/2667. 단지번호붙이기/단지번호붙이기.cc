9a5f1a9f1d781feb0c50af6f1083764d2e5d53c9
Jungmin5 <100908119+Jungmin5@users.noreply.github.com>
2024-03-25 06:39:07 +0900

    [Silver I] Title: 단지번호붙이기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
+int n;
+string board[27];
+bool vis[27][27];
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> board[i];
+    }
+    int count = 0;
+    vector<int> ans;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (board[i][j] == '0' || vis[i][j] == 1) continue;
+            queue<pair<int, int>> q;
+            vis[i][j] = 1;
+            q.push({i, j});
+            int width = 1;
+            count++;
+            while (!q.empty()) {
+                auto cur = q.front(); q.pop();
+                for (int dir = 0; dir < 4; dir++) {
+                    int nx = cur.first + dx[dir];
+                    int ny = cur.second + dy[dir];
+                    if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+                    if (board[nx][ny] == '0' || vis[nx][ny] == 1) continue;
+                    q.push({nx, ny});
+                    vis[nx][ny] = 1;
+                    width++;
+                }
+            }
+            ans.push_back(width);
+        }
+    }
+    cout << count << "\n";
+    sort(ans.begin(), ans.end());
+    for (int i : ans) cout << i << "\n";
+}
