9f91d1954469ee7a680790209a96b5a1b4cd023a
Jungmin5 <100908119+Jungmin5@users.noreply.github.com>
2024-04-22 03:52:59 +0900

    [Bronze III] Title: A+B - 6, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int a, b, t;
+char c;
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> t;
+    while (t--) {
+       cin >> a >> c >> b;
+       cout << a + b << "\n";
+    }
+}
