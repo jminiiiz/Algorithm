01185ac6e8e74af3eb998a5e827d11ae0273764f
Jungmin5 <100908119+Jungmin5@users.noreply.github.com>
2024-04-22 03:58:32 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int a, b, t;
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> t;
+    int c = 1;
+    while (t--) {
+       cin >> a >> b;
+       cout << "Case #" << c++ << ": " << a << " + " << b << " = " << a + b << "\n";
+    }
+}
