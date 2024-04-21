22829a53f0fc50184d971918f566f1a83d91b44f
Jungmin5 <100908119+Jungmin5@users.noreply.github.com>
2024-04-22 04:06:48 +0900

    [Bronze III] Title: 열 개씩 끊어 출력하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11721. 열 개씩 끊어 출력하기/열 개씩 끊어 출력하기.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    string s;
+    cin >> s;
+    for (int i = 0; i < s.size(); i++) {
+        cout << s[i];
+        if (i % 10 == 9) cout << "\n";
+    }
+}
