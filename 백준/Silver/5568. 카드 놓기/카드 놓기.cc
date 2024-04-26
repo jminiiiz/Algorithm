4bce08b2762ab043858787caac6c09bde9a1d253
Jungmin5 <100908119+Jungmin5@users.noreply.github.com>
2024-04-26 22:46:41 +0900

    [Silver IV] Title: 카드 놓기, Time: 0 ms, Memory: 2424 KB -BaekjoonHub

---
+++ 백준/Silver/5568. 카드 놓기/카드 놓기.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+unordered_set<string> US;
+vector<int> DECK;
+int N, P, cnt;
+bool USED[10];
+void f(vector<int> V, int D) {
+    if (D == P) {
+        string res = "";
+        for (int k = 0; k < P; k++)
+            res += to_string(DECK[k]);
+        US.insert(res);
+        return;
+    }
+    for (int i = 0; i < N; i++) {
+        if (!USED[i]) {
+            USED[i] = true;
+            DECK.push_back(V[i]);
+            f(V, D + 1);
+            DECK.pop_back();
+            USED[i] = false;
+        }
+    }
+}
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    cin >> N >> P;
+    vector<int> V(N);
+    for (int i = 0; i < N; i++) cin >> V[i];
+    f(V, 0);
+    for (auto s : US) cnt++;
+    cout << cnt;
+}
