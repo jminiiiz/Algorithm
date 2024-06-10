5a60be0bcbb1be4e66f80c4242100ce95876b253
jeoongmiin <100908119+jeoongmiin@users.noreply.github.com>
2024-06-11 00:43:00 +0900

    [Bronze I] Title: 세로읽기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+char arr[5][15];
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    for (int i = 0; i < 5; i++) {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < 15; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (arr[j][i] != NULL) {
+                cout << arr[j][i];
+            }
+        }
+    }
+}
