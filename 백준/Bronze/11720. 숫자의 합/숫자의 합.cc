38d6906d74210bc9ef7da70d8d3ce7f96fe9b4f0
jeoongmiin <100908119+jeoongmiin@users.noreply.github.com>
2024-06-10 23:34:59 +0900

    [Bronze IV] Title: 숫자의 합, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.cc
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.cc
@@ -5,8 +5,8 @@ string s;
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
     cin >> n >> s;
-    for (int i = 0; i < n; i++)  {
-        cnt += s[i] - '0';
+    for (char a : s) {
+        cnt += a - '0';
     }
     cout << cnt;
 }
