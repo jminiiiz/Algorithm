6b85928ab2578edf6f7b703df3e2365dcf109e9f
안정민 <100908119+jminiiiz@users.noreply.github.com>
2024-07-14 01:12:44 +0900

    [Silver IV] Title: 듣보잡, Time: 32 ms, Memory: 9868 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, m;
+vector<string> notHeard;
+vector<string> notSeen;
+vector<string> ans;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    cin >> n >> m;
+    while (n--) {
+        string s;
+        cin >> s;
+        notHeard.push_back(s);
+    }
+    while (m--) {
+        string s;
+        cin >> s;
+        notSeen.push_back(s);
+    }
+    sort(notHeard.begin(), notHeard.end());
+    sort(notSeen.begin(), notSeen.end());
+    set_intersection(notHeard.begin(), notHeard.end(), notSeen.begin(), notSeen.end(), back_inserter(ans));
+    cout << ans.size() << "\n";
+    for (auto s : ans) cout << s << "\n";
+}
