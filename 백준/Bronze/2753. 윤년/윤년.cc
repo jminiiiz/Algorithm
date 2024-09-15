2d7a9fd4096e4d7de37f78e9f81d80f4b8144ef7
안정민 <100908119+jminiiiz@users.noreply.github.com>
2024-09-16 05:01:05 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.cc
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -1,10 +1,8 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-
 int main() {
     int n;
     cin >> n;
     if ((n % 4 == 0 and n % 100 != 0) or (n % 400 == 0)) cout << "1";
     else cout << "0";
-    return 0;
 }
