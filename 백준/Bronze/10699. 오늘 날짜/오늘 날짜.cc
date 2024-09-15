ed9f312cae4e07b8eafd8bb203792a18efc506ab
안정민 <100908119+jminiiiz@users.noreply.github.com>
2024-09-16 03:53:09 +0900

    [Bronze V] Title: 오늘 날짜, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10699. 오늘 날짜/오늘 날짜.cc
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.cc
@@ -1,10 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int main()
-{
+int main() {
     time_t timer = time(nullptr);
     struct tm* t = localtime((&timer));
-
-        printf("%d-%02d-%02d",t->tm_year + 1900, t->tm_mon + 1, t->tm_mday + 1); // 서버시간과 현재시간 차이로 인해 날짜에 + 1
+    printf("%d-%02d-%02d",t->tm_year + 1900, t->tm_mon + 1, t->tm_mday + 1); 
 }
