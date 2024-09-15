77e75e13fac65a6630c1cf4ac9c8ecfacec2056a
안정민 <100908119+jminiiiz@users.noreply.github.com>
2024-09-16 04:04:58 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.cc
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -1,11 +1,9 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-
 int main() {
     double A, B;
     cin >> A >> B;
     cout.precision(10);
     cout << fixed;
     cout << A / B;
-    return 0;
 }
