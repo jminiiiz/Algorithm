24afde932c7d7f63d233baeb618dcb78eb06b803
안정민 <100908119+jminiiiz@users.noreply.github.com>
2024-09-16 04:40:40 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.cc
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -1,13 +1,11 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-
 int main() {
     int A, B;
     cin >> A >> B;
-    cout << A + B << endl;
-    cout << A - B << endl;
-    cout << A * B << endl;
-    cout << A / B << endl;
-    cout << A % B << endl;
-    return 0;
+    cout << A + B << "\n";
+    cout << A - B << "\n";
+    cout << A * B << "\n";
+    cout << A / B << "\n";
+    cout << A % B << "\n";
 }
