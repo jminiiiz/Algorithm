3dece8a2bd49287ec5c2e00e66534ff2c2567578
안정민 <100908119+jminiiiz@users.noreply.github.com>
2024-10-04 03:42:02 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 296 ms, Memory: 3972 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, m;
+int a[500005];
+int lower_idx(int target, int len) {
+    int st = 0;
+    int en = len;
+    while (st < en) {
+        int mid = (st + en) / 2;
+        if (a[mid] >= target) en = mid;
+        else st = mid + 1;
+    }
+    return st;
+}
+int upper_idx(int target, int len) {
+    int st = 0;
+    int en = len;
+    while (st < en) {
+        int mid = (st + en) / 2;
+        if (a[mid] > target) en = mid;
+        else st = mid + 1;
+    }
+    return st;
+}
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    cin >> n;
+    for (int i = 0; i < n; i++) cin >> a[i];
+    sort(a, a+n);
+    cin >> m;
+    while (m--) {
+        int t;
+        cin >> t;
+        cout << upper_idx(t, n) - lower_idx(t, n) << ' ';
+    }
+}
