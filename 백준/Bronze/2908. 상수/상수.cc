3a80c1b05c9a6e499f2d784117e155632310e338
안정민 <100908119+jminiiiz@users.noreply.github.com>
2024-10-03 22:52:33 +0900

    [Bronze II] Title: 상수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.cc
+++ 백준/Bronze/2908. 상수/상수.cc
@@ -1,45 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
-int a, b, res;
+int reverseNumber(int n) {
+    int res = 0;
+    while (n > 0) {
+        res = res * 10 + n % 10;
+        n /= 10;
+    }
+    return res;
+}
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
+    int a, b;
     cin >> a >> b;
-    if (a % 10 == b % 10) {
-        if ((a / 10) % 10 == (b / 10) % 10) {
-            if (a / 100 > b / 100) {
-                res += (a % 10) * 100;
-                a /= 10;
-                res += (a % 10) * 10;
-                res += (a /= 10);
-            }
-            else {
-                res += (b % 10) * 100;
-                b /= 10;
-                res += (b % 10) * 10;
-                res += (b /= 10);
-            }
-        }
-        else if ((a / 10) % 10 > (b / 10) % 10) {
-            res += (a % 10) * 100;
-            a /= 10;
-            res += (a % 10) * 10;
-            res += (a /= 10);
-        } else {
-            res += (b % 10) * 100;
-            b /= 10;
-            res += (b % 10) * 10;
-            res += (b /= 10);
-        }
-    } else if (a % 10 > b % 10) {
-        res += (a % 10) * 100;
-        a /= 10;
-        res += (a % 10) * 10;
-        res += (a /= 10);
-    } else {
-        res += (b % 10) * 100;
-        b /= 10;
-        res += (b % 10) * 10;
-        res += (b /= 10);
-    }
-    cout << res;
+    cout << max(reverseNumber(a), reverseNumber(b));
 }
