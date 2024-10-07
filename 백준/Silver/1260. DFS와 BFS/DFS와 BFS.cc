8b618bd8d59101fb96f93893147b9f2e9fecd526
안정민 <100908119+jminiiiz@users.noreply.github.com>
2024-10-08 06:22:04 +0900

    [Silver II] Title: DFS와 BFS, Time: 0 ms, Memory: 2312 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, m, st;
+vector<int> adj[1001];
+bool vis[1001];
+
+void dfs(int cur) {
+    vis[cur] = true;
+    cout << cur << ' ';
+    for (auto nxt : adj[cur]) {
+        if (vis[nxt]) continue;
+        dfs(nxt);
+    }
+}
+
+void bfs() {
+    queue<int> q;
+    q.push(st);
+    vis[st] = true;
+    while (!q.empty()) {
+        int cur = q.front(); q.pop();
+        cout << cur << ' ';
+        for (auto nxt : adj[cur]) {
+            if (vis[nxt]) continue;
+            q.push(nxt);
+            vis[nxt] = true;
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    cin >> n >> m >> st;
+    while (m--) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    for (int i = 1; i <= n; i++)
+        sort(adj[i].begin(), adj[i]. end());
+    dfs(st);
+    cout << '\n';
+    fill(vis+1, vis+n+1, false);
+    bfs();
+}
