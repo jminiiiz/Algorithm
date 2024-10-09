bd2d23c11caecb3ec3453855845b78b29ccc3314
안정민 <100908119+jminiiiz@users.noreply.github.com>
2024-10-10 04:07:33 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 32 ms, Memory: 4752 KB -BaekjoonHub

---
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+int v, e, st, en;
+vector<pair<int, int>> adj[1005];
+const int INF = 0x3f3f3f3f;
+int d[1005];
+int pre[1005];
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    cin >> v >> e;
+    fill(d, d+v+1, INF);
+    while (e--) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        adj[u].push_back({w, v});
+    }
+    cin >> st >> en;
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    d[st] = 0;
+    pq.push({d[st], st});
+    while (!pq.empty()) {
+        auto cur = pq.top(); pq.pop();
+        if (d[cur.second] != cur.first) continue;
+        for (auto nxt : adj[cur.second]) {
+            if (d[nxt.second] <= d[cur.second] + nxt.first) continue;
+            d[nxt.second] = d[cur.second] + nxt.first;
+            pq.push({d[nxt.second], nxt.second});
+            pre[nxt.second] = cur.second;
+        }
+    }
+    cout << d[en] << '\n';
+    vector<int> path;
+    int cur = en;
+    while (cur != st) {
+        path.push_back(cur);
+        cur = pre[cur];
+    }
+    path.push_back(cur);
+    reverse(path.begin(), path.end());
+    cout << path.size() << '\n';
+    for (auto x : path) cout << x << ' ';
+}
