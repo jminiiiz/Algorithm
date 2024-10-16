bdfc471a3ca4948a7f477bc899294800a05eb3cb
안정민 <100908119+jminiiiz@users.noreply.github.com>
2024-10-16 23:51:16 +0900

    [Bronze IV] Title: $A + B - C$, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.cc
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int a, b, c;
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    cin >> a >> b >> c;
+    cout << a + b - c << '\n';
+    cout << stoi(to_string(a) + to_string(b)) - c << '\n';
+}
