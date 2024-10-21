3d9f2ac59abb246e319fbd6587019093ff3fc033
안정민 <100908119+jminiiiz@users.noreply.github.com>
2024-10-22 01:20:28 +0900

    [Silver II] Title: 유기농 배추, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.cc
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -1,46 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
-int t, m, n, k;
+int t, n, m, k, board[51][51];
+bool vis[51][51];
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
-int vis[52][52];
-bool board[52][52];
-
+void dfs(int x, int y) {
+    vis[x][y] = true;
+    for (int dir = 0; dir < 4; dir++) {
+        int nx = x + dx[dir];
+        int ny = y + dy[dir];
+        if (nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
+        if (board[nx][ny] == 1 && !vis[nx][ny]) {
+            dfs(nx, ny);
+        }
+    }
+}
 int main() {
-    ios::sync_with_stdio(0); cin.tie(0);
     cin >> t;
     while (t--) {
         cin >> m >> n >> k;
         while (k--) {
             int x, y;
             cin >> x >> y;
-            board[y][x] = true;
+            board[x][y] = 1;
         }
-        int cnt = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (vis[i][j] || !board[i][j]) continue;
-                queue<pair<int, int>> q;
-                q.push({i, j});
-                vis[i][j] = 1;
-                while (!q.empty()) {
-                    auto cur = q.front(); q.pop();
-                    for (int dir = 0; dir < 4; dir++) {
-                        int nx = cur.first + dx[dir];
-                        int ny = cur.second + dy[dir];
-                        if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-                        if (vis[nx][ny] || !board[nx][ny]) continue;
-                        vis[nx][ny] = 1;
-                        q.push({nx, ny});
-                    }
+        int ret = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 1 && !vis[i][j]) {
+                    ret++;
+                    dfs(i, j);
                 }
-                cnt++;
             }
         }
-        cout << cnt << "\n";
-        for (int i = 0; i < n; i++) {
-            fill(board[i], board[i] + m, 0);
-            fill (vis[i], vis[i] + m, false);
+        cout << ret << '\n';
+        for (int i = 0; i < m; i++) {
+            fill(board[i], board[i] + n, 0);
+            fill(vis[i], vis[i] + n, false);
         }
     }
 }
