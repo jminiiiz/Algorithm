ada9345b3f10e568828023ca8c78da12239135a4
안정민 <100908119+jminiiiz@users.noreply.github.com>
2024-10-24 02:04:40 +0900

    [Silver III] Title: 수열, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.cc
+++ 백준/Silver/2559. 수열/수열.cc
@@ -1,17 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, k, tmp, psum[100001], ret = -1000000;
+int n, k, tmp, res = -10000000, psum[100004];
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
+    ios::sync_with_stdio(0); cin.tie(0);
     cin >> n >> k;
     for (int i = 1; i <= n; i++) {
         cin >> tmp;
-        psum[i] = psum[i - 1] + tmp;
+        psum[i] = psum[i-1] + tmp;
     }
     for (int i = k; i <= n; i++) {
-        ret = max(ret, psum[i] - psum[i - k]);
+        res = max(res, psum[i] - psum[i-k]);
     }
-    cout << ret << "\n";
-    return 0;
+    cout << res;
 }
