8a06e8ed511b987df650a88f2d94ef31f4ad84a7
안정민 <100908119+jminiiiz@users.noreply.github.com>
2024-12-23 13:33:33 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.03 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.cpp
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.cpp
@@ -4,13 +4,12 @@
 using namespace std;
 
 long long solution(string numbers) {
-    string snum[10] = { "zero", "one", "two", "three", "four", 
+    string num[10] = { "zero", "one", "two", "three", "four", 
                       "five", "six", "seven", "eight", "nine" };
-    string inum[10] = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
     for (int i = 0; i < 10; i++) {
         size_t pos = 0;
-        while ((pos = numbers.find(snum[i], pos)) != string::npos) {
-        	numbers.replace(pos, snum[i].length(), inum[i]);
+        while ((pos = numbers.find(num[i], pos)) != string::npos) {
+        	numbers.replace(pos, num[i].length(), to_string(i));
         }
     }
     return stol(numbers);
