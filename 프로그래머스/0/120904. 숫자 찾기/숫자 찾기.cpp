3f0e4afbcf7a569b2f27f983e97909ec0e473cf4
안정민 <100908119+jminiiiz@users.noreply.github.com>
2024-12-24 16:06:12 +0900

    [level 0] Title: 숫자 찾기, Time: 0.02 ms, Memory: 3.64 MB -BaekjoonHub

---
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num, int k) {
+    string s = to_string(num);
+    for (int i = 0; i < s.size(); i++) {
+        if ((s[i] - '0' == k)) {
+            return i + 1;
+        }
+    }
+    return -1;
+}
