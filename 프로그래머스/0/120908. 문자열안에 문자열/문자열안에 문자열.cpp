47caa643f94d80dfc2d541d1e3384c2759373b43
안정민 <100908119+jminiiiz@users.noreply.github.com>
2024-12-26 13:07:47 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.cpp
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.cpp
@@ -5,6 +5,5 @@ using namespace std;
 
 int solution(string str1, string str2) {
     int n = str1.find(str2);
-    if (n == string::npos) return 2;
-    else return 1;
+    return (n == string::npos) ? 2 : 1;
 }
