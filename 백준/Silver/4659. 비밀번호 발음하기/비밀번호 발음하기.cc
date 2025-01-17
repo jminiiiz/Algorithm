9b93b18aa06635270a7a04f441e21de8822a2114
안정민 <100908119+jminiiiz@users.noreply.github.com>
2025-01-18 04:06:56 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.cc
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.cc
@@ -1,27 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 string s;
-int lcnt, vcnt;
 bool isVowel(int idx) {
-    return (idx == 'a' || idx == 'e' || idx == 'i' || idx == 'o' || idx == 'u');
+    return idx == 'a' || idx == 'e' || idx == 'i' || idx == 'o' || idx == 'u';
 }
 int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
     while (true) {
         cin >> s;
         if (s == "end") break;
-        lcnt = vcnt = 0;
-        bool flag = 0;
-        bool is_include_v = 0;
+        int l = 0, v = 0;
+        bool flag = 0, isInclude = 0;
         int prev = -1;
         for (int i = 0; i < s.size(); i++) {
             int idx = s[i];
-            if (isVowel(idx)) lcnt++, vcnt = 0, is_include_v = 1;
-            else vcnt++, lcnt = 0;
-            if (vcnt == 3 || lcnt == 3) flag = 1;
+            if (isVowel(idx)) l++, v = 0, isInclude = 1;
+            else v++, l = 0;
+            if (v == 3 || l == 3) flag = 1;
             if (i >= 1 && (prev == idx) && (idx != 'e' && idx != 'o')) flag = 1;
             prev = idx;
         }
-        if (is_include_v == 0) flag = 1;
+        if (!isInclude) flag = 1;
         if (flag) cout << "<" << s << "> is not acceptable.\n";
         else cout << "<" << s << "> is acceptable.\n";
     }
